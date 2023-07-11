1299a4278db2419ee52d9275f63a705fff43314e
TahsinMostafiz <mostafizur.webdesigner131@gmail.com>
2023-07-11 10:28:40 +0600

    added textarea section and Some changes have been made

--- src/componets/Content.jsx
+++ src/componets/Content.jsx
@@ -34,16 +34,25 @@ const Content = () => {
 
 ]
     const [text, setText] = useState({})
-    console.log(text);
-    const showText = () => {
+    const [displayText, setDisplayText] = useState('');
+
+    const handleShowText = () => {
        let randomQuote = Math.floor(Math.random() * loveQuotes.length);
        setText(loveQuotes[randomQuote])
     }
+
+    
+    const handleDisplayText = (e) => {
+        e.preventDefault();
+       const text = e.target.inputText.value;
+        setDisplayText(text)
+    }
+
     return (
-        <section className="px-7 py-16 bg-gray-200 text-gray-900 ">
+        <section className="px-7 py-16 bg-gray-200 text-gray-900 lg:h-[75vh]">
             <div className="container mx-auto flex flex-col overflow-hidden lg:flex-row gap-2">
                 <div className="w-full md:w-1/2 px-2 lg:px-5">
-                    <button type="button" className="font-semibold rounded bg-red-400 text-white px-6 py-2 my-3" onClick={showText}>Get Quote</button>
+                    <button type="button" className="font-semibold rounded bg-red-400 text-white px-6 py-2 my-3" onClick={handleShowText}>Get Quote</button>
                     {Object.entries(text).length === 0 ?<div className="bg-gray-300 rounded-lg p-5">
                         <p className="text-2xl text-justify">Please press the button to get a Love Quote! ♥♥♥</p>
                     </div>: <div className="bg-gray-300 rounded-lg p-5">
@@ -52,8 +61,18 @@ const Content = () => {
                     </div>}
                 </div>
                 <div className="w-full md:w-1/2">
-                    <img src="https://www.floraqueen.com/blog/wp-content/uploads/2020/03/shutterstock_261847937.jpg" alt="" className=" w-full rounded-lg" />
+                    <form onSubmit={handleDisplayText} >
+                    <div className="col-span-full">
+                        <label  className="text-lg font-bold pt-5 pb-4 block">Provide Text</label>
+                        <textarea  id="inputText" name="inputText" placeholder="" className="w-full h-32 rounded-md focus:ring focus:ri focus:ri text-gray-900 text-base font-semibold p-2"></textarea>
+				    </div>
+                        <button type="submit" className="font-semibold rounded bg-red-400 text-white px-6 py-2 my-3">Display Text</button>
+                </form>
+                {displayText && <div className="bg-gray-300 rounded-lg p-5">
+                        <p className="text-2xl text-justify">{displayText}</p>
+                    </div>}
                 </div>
+                    
             </div>
 
         </section>
